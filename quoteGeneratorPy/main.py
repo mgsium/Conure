44858c4c4756b8b3766ff89a4305f3ef36a7fcfe
Musab Guma'a <musabgumaa@gmail.com>
2020-06-30 20:01:32 +0100

    Removed pygenerator

--- quoteGeneratorPy/main.py
+++
@@ -1,8 +0,0 @@
-import csv
-import json
-
-filename = "./quotes.txt"
-
-with open(filename, "r") as file:
-    csvReader = csv.reader(file, delimiter=",")
-    quotes = [[line[0], line[1]] for line in csvReader]
